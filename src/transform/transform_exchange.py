2c1e603c1d0bebce7ab02e26c4bcf6cfdb155332
Felipe <dosanjosfelipe06@gmail.com>
2026-03-15 12:20:16 -0300

    bug: fix path bug and duplicate log bug

--- src/transform/transform_exchange.py
+++ src/transform/transform_exchange.py
@@ -1,11 +1,8 @@
 import logging
 import pandas as pd
 from pathlib import Path
-from utils.logging_config import setup_logging
 
-setup_logging()
 logger = logging.getLogger(__name__)
-
 
 def transform_exchange_data(path_name: Path) -> pd.DataFrame:
     if not path_name.exists():
@@ -34,7 +31,7 @@ def transform_exchange_data(path_name: Path) -> pd.DataFrame:
 
 
 def save_staging_exchange_data(df: pd.DataFrame) -> None:
-    output_path = Path(__file__).parent.parent.parent / 'data' / 'staging' / 'exchange_data.parquet'
+    output_path = Path(__file__).parent.parent.parent / 'data' / 'processed' / 'exchange.parquet'
     output_path.parent.mkdir(parents=True, exist_ok=True)
 
     df.to_parquet(output_path, index=False)
@@ -42,6 +39,3 @@ def save_staging_exchange_data(df: pd.DataFrame) -> None:
     relative_path = str(output_path).split("data/", 1)[1]
 
     logger.info(f'Arquivo salvo em {relative_path}')
-
-df1 = transform_exchange_data(Path('../../data/raw/exchange_data.json'))
-save_staging_exchange_data(df1)
